b940b67f5a1421000c4e062dd112eaec20c7442c
Ismail Muhammad <ismaeelmuhammad123@gmail.com>
2025-04-05 08:50:55 +0100

    some and .env

--- src/lib/firebase.tsx
+++ src/lib/firebase.tsx
@@ -24,13 +24,14 @@ import { getFirestore, collection, addDoc, deleteDoc, getDocs, doc } from "fireb
 // };
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBBy2_c-dIshazL4Lo4gfop8cuGxfHwAmI",
-    authDomain: "tubali.firebaseapp.com",
-    projectId: "tubali",
-    storageBucket: "tubali.firebasestorage.app",
-    messagingSenderId: "39142563758",
-    appId: "1:39142563758:web:0fe52c273ae4183bf5323f"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
